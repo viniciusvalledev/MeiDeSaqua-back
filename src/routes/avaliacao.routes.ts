4d1a932672583b2dc13613bedd14dd64c5b8e548
Vinicius Valle dev <vnthedeveloper@gmail.com>
2025-09-18 12:48:19 -0300

    Fixed: "avalições fixed"

--- src/routes/avaliacao.routes.ts
+++ src/routes/avaliacao.routes.ts
@@ -1,7 +1,7 @@
 // src/routes/avaliacao.routes.ts
 import { Router } from 'express';
 import AvaliacaoController from '../controllers/AvaliacaoController';
-// import { authMiddleware } from '../middlewares/auth.middleware';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
@@ -9,8 +9,8 @@ const router = Router();
 router.get('/estabelecimento/:id', AvaliacaoController.listarPorEstabelecimento);
 
 // Rotas que precisam de autenticação
-router.post('/', /* authMiddleware, */ AvaliacaoController.submeterAvaliacao);
-router.put('/:id', /* authMiddleware, */ AvaliacaoController.atualizarAvaliacao);
-router.delete('/:id', /* authMiddleware, */ AvaliacaoController.excluirAvaliacao);
+router.post('/', authMiddleware, AvaliacaoController.submeterAvaliacao);
+router.put('/:id', authMiddleware, AvaliacaoController.atualizarAvaliacao);
+router.delete('/:id', authMiddleware, AvaliacaoController.excluirAvaliacao);
 
 export default router;
