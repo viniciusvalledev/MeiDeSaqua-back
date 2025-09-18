c6c1ae98ed8476ffa357e8628a93a27482ca9951
Vinicius Valle dev <vnthedeveloper@gmail.com>
2025-09-18 10:27:24 -0300

    estabelecimento api is off

--- src/routes/estabelecimento.routes.ts
+++ src/routes/estabelecimento.routes.ts
@@ -4,10 +4,10 @@ import EstabelecimentoController from '../controllers/EstabelecimentoController'
 
 const router = Router();
 
-router.get('/', EstabelecimentoController.listarTodos);
-router.get('/buscar', EstabelecimentoController.buscarPorNome); // Ex: /buscar?nome=barbearia
+router.get('/estabelecimentos', EstabelecimentoController.listarTodos);
+router.get('/buscar', EstabelecimentoController.buscarPorNome); 
 router.get('/:id', EstabelecimentoController.buscarPorId);
-router.post('/', EstabelecimentoController.cadastrar);
+router.post('/estabelecimentos', EstabelecimentoController.cadastrar);
 router.post('/:id/status', EstabelecimentoController.alterarStatus);
 
 export default router;
