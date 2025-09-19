98c168ac57728be5c7f04320d1f87780c4bbbcf9
Vinícius Diller <viniciusdiller@hotmail.com>
2025-09-19 15:22:11 -0300

    Feat: Excluir comentario

--- src/routes/avaliacao.routes.ts
+++ src/routes/avaliacao.routes.ts
@@ -1,16 +1,19 @@
 // src/routes/avaliacao.routes.ts
-import { Router } from 'express';
-import AvaliacaoController from '../controllers/AvaliacaoController';
-import { authMiddleware } from '../middlewares/auth.middleware';
+import { Router } from "express";
+import AvaliacaoController from "../controllers/AvaliacaoController";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 // Rota pública para listar avaliações de um estabelecimento
-router.get('/estabelecimento/:id', AvaliacaoController.listarPorEstabelecimento);
+router.get(
+  "/estabelecimento/:id",
+  AvaliacaoController.listarPorEstabelecimento
+);
 
 // Rotas que precisam de autenticação
-router.post('/', authMiddleware, AvaliacaoController.submeterAvaliacao);
-router.put('/:id', authMiddleware, AvaliacaoController.atualizarAvaliacao);
-router.delete('/:id', authMiddleware, AvaliacaoController.excluirAvaliacao);
+router.post("/", authMiddleware, AvaliacaoController.submeterAvaliacao);
+router.put("/:id", authMiddleware, AvaliacaoController.atualizarAvaliacao);
+router.delete("/:id", authMiddleware, AvaliacaoController.excluirAvaliacao);
 
 export default router;
